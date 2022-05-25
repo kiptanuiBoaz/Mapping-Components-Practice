c764cd198ac6686077f9f7271750304d19d15a49
boaz <boaserem022@gmail.com>
2022-05-25 12:25:28 +0300

    first from local

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,32 @@
 import React from "react";
+import Entry from "./Entry"
+import emojipedia from "../emojipedia";
+
+function createEmoji (emojipedia){
+  return(
+    <div>
+     
+      <Entry
+        key = {emojipedia.id}
+        id = {emojipedia.id}
+        emoji = {emojipedia.emoji}
+        name = {emojipedia.name}
+        meaning = {emojipedia.meaning}
+      />
+    </div>
+    
+  )
+};
 
 function App() {
   return (
     <div>
       <h1>
-        <span>emojipedia</span>
+      <span>emojipedia</span>
       </h1>
-
-      <dl className="dictionary">
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
-      </dl>
+      {emojipedia.map(createEmoji)};
     </div>
+   
   );
 }
 
---
+++ src/components/Entry.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+// import emojipedia from "../emojipedia"
+
+
+function Entry(props){
+    return(
+        <div>
+            <div>
+                
+
+                <dl className="dictionary">
+                    <div className="term">
+                        <dt>
+                            <span className="emoji" role="img" aria-label="Tense Biceps">
+                                {props.emoji}
+                            </span>
+                            <span>{props.name}</span>
+                        </dt>
+                        <dd>
+                            {props.meaning}
+                        </dd>
+                    </div>
+                
+                </dl>
+            </div>
+        </div>
+    )
+}
+
+export default Entry;
